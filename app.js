bab0b718bd1874f87243453268452178ad6497ca
mdavidrodriguez <mdavidrodriguez@unicesar.edu.co>
2023-12-01 10:15:15 -0500

    estructura del proyecto ' MV'C

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const cors = require("cors")
 const app = express()
+require("dotenv").config()
 
+// Evitar error de origenes cruzados en el navegador
 app.use(cors())
 
+// Puerto de la app
+const port = process.env.PORT || 4000
 
-const port = 3000
-
+// Inicialización de la aplicación
 app.listen(port, () => {
     console.log('Tu app esta lista por http://localhost:' + port);
 })
